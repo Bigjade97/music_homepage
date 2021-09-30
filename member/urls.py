507a1b12e0809f156431342db99b6d8303247597
taekyu <ptk1997@naver.com>
2021-10-01 03:33:52 +0900

    9 / 30 마이페이지 마무리, 뮤직차트 페이지 초반

--- member/urls.py
+++ member/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
         path('id_check', views.id_check),
         path('out', views.out),
         path('pw_chg', views.pw_chg),
+        path('chart_top', views.chart_top),
+        path('chart_genre', views.chart_genre),
+        path('chart_genre_pop', views.chart_genre_pop),
+        path('chart_genre_ost', views.chart_genre_ost),
+        path('chart_genre_trot', views.chart_genre_trot),
+        path('chart_video', views.chart_video),
 ]
--- member/views.py
+++ member/views.py
@@ -9,7 +9,7 @@ def main (req) :
     return render (req, 'music.html', {'pam1' : req.session['userid']} )
   else:
     if req.method == "POST" :
-      logged_member = User.objects.filter(userid = req.POST.get('id'), password = req.POST.get('pw'))
+      logged_member = User.objects.get(userid = req.POST.get('id'), password = req.POST.get('pw'))
       if logged_member :
         req.session['userid'] = req.POST.get('id')
         req.session['password'] = req.POST.get('pw')
@@ -35,7 +35,7 @@ def make (req) :
 
 def login (req) :
   if req.method == "POST" :
-    logged_member = User.objects.filter(userid = req.POST.get('id'), password = req.POST.get('pw'))
+    logged_member = User.objects.get(userid = req.POST.get('id'), password = req.POST.get('pw'))
     if logged_member :
       req.session['userid'] = req.POST.get('id')
       req.session['password'] = req.POST.get('pw')
@@ -67,10 +67,30 @@ def mypage (req) :
   return render(req, 'mypage.html', {'pam1' : req.session['userid']}) 
 
 def edit (req) :
-  return render(req, 'edit.html', {'pam1' : req.session['userid'], 'pam2' : req.session['password']}) 
+  if req.method == "POST" :
+    edit_member = User.objects.get(userid = req.session.get('userid'))
+    edit_member.username = req.POST.get('name')
+    edit_member.gender = req.POST.get('gender')
+    edit_member.address = req.POST.get('address')
+    edit_member.like = req.POST.get('genre')
+    edit_member.save()
+    messages.success(req, '회원정보 수정이 완료되었습니다.')
+    print(edit_member.like)
+    return render(req, 'edit.html', {'pam1' : edit_member.userid, 'pam2' : edit_member.password, 'pam3' : edit_member.username, 'pam4' : edit_member.gender, 'pam5' : edit_member.address, 'pam6' : edit_member.like}) 
+  else:
+    edit_member = User.objects.get(userid = req.session.get('userid'))
+    return render(req, 'edit.html', {'pam1' : edit_member.userid, 'pam2' : edit_member.password, 'pam3' : edit_member.username, 'pam4' : edit_member.gender, 'pam5' : edit_member.address, 'pam6' : edit_member.like}) 
 
 def pw_chg (req) :
-  return render(req, 'pw_chg.html', {'pam1' : req.session['userid']})
+  if req.method == "POST" :
+    edit_member = User.objects.get(userid = req.session.get('userid'))
+    edit_member.password = req.POST.get('pw')
+    edit_member.save()
+    messages.success(req, '비밀번호 변경이 완료되었습니다.')
+    return render(req, 'pw_chg.html', {'pam1' : req.session['userid']})
+  else :
+    return render(req, 'pw_chg.html', {'pam1' : req.session['userid']})
+
 
 def out (req) :
   if req.method == "POST" :
@@ -79,8 +99,28 @@ def out (req) :
     if user:
       user.delete()
       req.session.pop('userid')
+      messages.success(req, '탈퇴가 완료되었습니다.')
       return redirect('../ssac/main')
     else :
-      return render(req, 'out.html') 
+      messages.error(req, '비밀번호가 일치하지 않습니다.')
+      return render(req, 'out.html', {'pam1' : req.session['userid']}) 
   else:
     return render (req, 'out.html', {'pam1' : req.session['userid']})
+
+def chart_top (req) :
+  return render(req, 'chart_top.html', {'pam1' : req.session['userid']}) 
+
+def chart_genre (req) :
+  return render(req, 'chart_genre.html', {'pam1' : req.session['userid']}) 
+
+def chart_genre_pop (req) :
+  return render(req, 'chart_genre_pop.html', {'pam1' : req.session['userid']}) 
+
+def chart_genre_ost (req) :
+  return render(req, 'chart_genre_ost.html', {'pam1' : req.session['userid']}) 
+
+def chart_genre_trot (req) :
+  return render(req, 'chart_genre_trot.html', {'pam1' : req.session['userid']}) 
+
+def chart_video (req) :
+  return render(req, 'chart_video.html', {'pam1' : req.session['userid']}) 
